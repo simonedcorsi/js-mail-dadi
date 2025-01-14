5162a43b8a1c01dd3827eccd75b05b35a2b13123
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-14 17:17:42 +0100

    set dadi lancio

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -4,7 +4,9 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 // Generazione numero random da 1 a 6
+let lancioDado = Math.floor((Math.random() * 6) + 1);
 
 // Lancio del giocatore e del PC
+
 
 // Stabilire il vincitore 
