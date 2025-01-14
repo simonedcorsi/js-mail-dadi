2fcabf283cbc3ed37263936dbfff536cc9552452
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-14 15:27:24 +0100

    set

---
+++ main.js
@@ -0,0 +1,26 @@
+/* Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota:
+Non è necessario provvedere alla validazione delle email
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+*/
+
+// MAIL
+// Creazione lista e-mail di invitati
+
+
+// Chiedere e-mail all'utente
+
+
+// Controllo se utente è nella lista
+
+
+// Stampare esito controllo
