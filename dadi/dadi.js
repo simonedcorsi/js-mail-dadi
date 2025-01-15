eb897446b4c140bb9acfa36cb92e102adad71900
simonedcorsi <simonedomenico.corsi@gmail.com>
2025-01-15 12:45:08 +0100

    dadi

--- dadi/dadi.js
+++ dadi/dadi.js
@@ -4,9 +4,17 @@ Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
 
 // Generazione numero random da 1 a 6
-let lancioDado = Math.floor((Math.random() * 6) + 1);
+let numeroGiocatore = Math.floor(Math.random() * 6) + 1;
+let numeroPc = Math.floor(Math.random() * 6) + 1;
 
 // Lancio del giocatore e del PC
-
+console.log( numeroGiocatore, numeroPc )
 
 // Stabilire il vincitore 
+if( numeroGiocatore > numeroPc ){
+    console.log( `Il giocatore ha vinto: ${numeroGiocatore} - ${numeroPc}` )
+} else if(numeroGiocatore < numeroPc ){
+    console.log( `Il pc ha vinto: ${numeroGiocatore} - ${numeroPc}` )
+}else {
+    console.log( `Il giocatore e il pc hanno pareggiato: ${numeroGiocatore} - ${numeroPc}` )
+}
